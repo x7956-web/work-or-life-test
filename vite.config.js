43a3efd5c2aab77f6a4104687419284cc513d787
x7956-web <x795626@gmail.com>
2026-07-07 18:10:37 +0800

    Enable route chunk splitting

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,17 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/"
+  base: "/",
+  build: {
+    cssCodeSplit: true,
+    target: "es2019",
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("resultView")) return "result";
+          return undefined;
+        }
+      }
+    }
+  }
 });
